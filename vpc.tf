9ffef7e34716fa81f82cf05f40ce6a740fd3506a
Rogelio García <rogelio@192.168.0.8>
2022-03-18 22:37:35 -0500

    1: Create GKE Cluster using Terraform

---
+++ vpc.tf
@@ -0,0 +1,34 @@
+# Variables
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+variable "zone" {
+    description = "zone"
+}
+
+# Provider
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+# Networking resources
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project_id}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
