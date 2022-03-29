7663ab37806bacb531c036cddc5e2b7d3d57febd
Rogelio García <rogelio@Rogelios-MacBook-Pro.local>
2022-03-29 16:51:19 -0500

    2/5: Attempt at creating Airflow DAGs to run dataflow jobs

--- dags/transactional_gcs_to_bq.py
+++ dags/transactional_gcs_to_bq.py
@@ -1,40 +1,39 @@
 import datetime
 
 from airflow import models
-from airflow.contrib.operators.dataflow_operator import DataflowTemplateOperator
+from airflow.contrib.operators.dataflow_operator import \
+    DataflowTemplateOperator
 from airflow.utils.dates import days_ago
 
-
-bucket_path = "rg-acp-storage"
-project_id = "shaped-icon-344520"
-gce_zone = "us-central1-a"
+bucket_path = 'rg-acp-storage'
+project_id = 'shaped-icon-344520'
+gce_zone = 'us-central1-a'
 
 
 default_args = {
-    "start_date": days_ago(1),
-    "dataflow_default_options": {
-        "project": project_id,
-        "zone": gce_zone,
-        "tempLocation": f"gs://{bucket_path}/tmp/"
+    'start_date': days_ago(1),
+    'dataflow_default_options': {
+        'project': project_id,
+        'zone': gce_zone,
+        'tempLocation': f'gs://{bucket_path}/airflow-tmp/'
     },
 }
 
 with models.DAG(
-    "transactional_layer",
+    'transactional_layer',
     default_args=default_args,
     schedule_interval=None,
 ) as dag:
     dataflow_template_op = DataflowTemplateOperator(
         task_id='user-purchase-gcs-csv-to-bq',
         retries=0,
-        template='gs://rg-acp-storage/templates/transactional_purchase_gcs_csv_to_bq',
+        template=f'gs://{bucket_path}/templates/transactional_purchase_gcs_csv_to_bq',
         job_name='gcs-csv-to-bq-user-purchase',
-        parameters={'input': 'gs://rg-acp-storage/data/purchase_minimal.csv'},
+        parameters={'input': f'gs://{bucket_path}/data/purchase_minimal.csv'},
         dataflow_default_options={
-            "project": "shaped-icon-344520",
-            "stagingLocation": "gs://rg-acp-storage/staging",
-            "tempLocation": "gs://rg-acp-storage/temp",
-            "serviceAccountEmail": "tf-sa-931@shaped-icon-344520.iam.gserviceaccount.com"
+            'project': f'{project_id}',
+            'stagingLocation': f'gs://{bucket_path}/staging',
+            'tempLocation': f'gs://{bucket_path}/temp',
+            'serviceAccountEmail': f'tf-sa-931@{project_id}.iam.gserviceaccount.com'
         }
     )
-
